80a22adca78fad4b1b267b2c209ffeaf8e3b52eb
notDefaultCompanyName <a.ratkevi4@gmail.com>
2022-10-10 13:30:36 +0300

    feat: create empty react project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {
+  Routes,
+  // Route,
+  // Link
+} from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+      <Routes>{/* <Route path='/' element={<Home />} /> */}</Routes>
+    </>
+  )
+}
+
+export default App
